8eaec8f4ebffb89d0e1ec094a57b2f836cbc4201
Dmitry <ecio1476@gmail.com>
2017-07-11 15:20:57 +0800

    Solve Grasshopper-Summation

---
+++ Python/8kyu/grasshopper_summation.py
@@ -0,0 +1,11 @@
+"""
+Created on Sun Jul  11 15:13:17 2017
+
+@author: Dmitry White
+"""
+
+# TODO: Write a program that finds the summation of every number between 1 and num. 
+# The number will always be a positive integer greater than 0.
+
+def summation(n):
+    return n*(n+1)/2
