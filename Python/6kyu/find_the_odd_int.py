a0f44a6dac474a452f026fafa8455165ba2446e8
Dmitry <ecio1476@gmail.com>
2017-07-26 17:17:19 +0300

    Solve Find The Odd Int

---
+++ Python/6kyu/find_the_odd_int.py
@@ -0,0 +1,18 @@
+"""
+Created on Tue Jul  25 17:15:26 2017
+
+@author: Dmitry White
+"""
+
+# TODO:Given an array, find the int that appears an odd number of times.
+# There will always be only one integer that appears an odd number of times.
+
+def find_it(seq):
+    for item in seq:
+        times = 0
+        for check in seq:
+            if check == item:
+                times += 1
+        if times % 2 != 0:
+            break
+    return item
