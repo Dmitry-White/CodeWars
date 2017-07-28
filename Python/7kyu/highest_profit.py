eb9773515cbdaaa8a27295e37ed2a6c4e0efc691
Dmitry <ecio1476@gmail.com>
2017-07-29 00:43:35 +0300

    Solve The highest profit wins!

---
+++ Python/7kyu/highest_profit.py
@@ -0,0 +1,10 @@
+"""
+Created on Sat Jul  29 00:43:08 2017
+
+@author: Dmitry White
+"""
+
+# TODO: Write a function that returns both the minimum and maximum number of the given list/array.
+
+def min_max(lst):
+  return [min(lst), max(lst)]
