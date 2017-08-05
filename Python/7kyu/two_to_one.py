baadc3d7b3a44a847f37b64f0e92dd53574fcca8
Dmitry White <dmitry.b.white@gmail.com>
2017-08-06 00:03:01 +0300

    Refactor Two to One

--- Python/7kyu/two_to_one.py
+++ Python/7kyu/two_to_one.py
@@ -8,13 +8,6 @@ Created on Sat Aug  05 23:58:21 2017
 # Return a new sorted string, the longest possible, containing distinct letters,
 # each taken only once - coming from s1 or s2.
 
-def longest(s1, s2):
-    my_list = []
-    for char in s1:
-        if char not in my_list:
-            my_list.append(char)
-    for char in s2:
-        if char not in my_list:
-            my_list.append(char)
-        my_list.sort()
-    return "".join(my_list)
+
+def longest(a1, a2):
+    return "".join(sorted(set(a1 + a2)))
