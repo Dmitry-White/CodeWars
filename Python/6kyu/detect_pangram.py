3004b46f826d183caa5d73cdcab149072001a159
Dmitry White <dmitry.b.white@gmail.com>
2017-08-09 23:47:32 +0300

    Correct Task Description

--- Python/6kyu/detect_pangram.py
+++ Python/6kyu/detect_pangram.py
@@ -4,7 +4,7 @@ Created on Wed Aug 09 23:45:41 2017
 @author: Dmitry White
 """
 
-# A pangram is a sentence that contains every single letter of the alphabet at least once.
+# TODO: A pangram is a sentence that contains every single letter of the alphabet at least once.
 # For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram,
 # because it uses the letters A-Z at least once (case is irrelevant).
 # Given a string, detect whether or not it is a pangram. Return True if it is,
