0d5bb96cb501f4e43f0b9674c67e685825282527
Dmitry White <dmitry.b.white@gmail.com>
2017-08-23 23:20:19 +0300

    Refactor Even or Odd

--- C++/even_odd.cpp
+++ C++/even_odd.cpp
@@ -12,12 +12,5 @@ Created on Wed Aug  23 23:18:11 2017
 
 std::string even_or_odd(int number)
 {
-  if(number%2 == 0)
-  {
-    return "Even";
-  }
-  else
-  {
-    return "Odd";
-  }
+  return (number%2==0)?"Even":"Odd";
 }
