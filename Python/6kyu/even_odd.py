0f182d81a46aea7e27846ab2c2d352b7a086b8ee
Dmitry White <dmitry.b.white@gmail.com>
2017-08-23 23:29:28 +0300

    Solve Even or Odd

---
+++ Python/6kyu/even_odd.py
@@ -0,0 +1,12 @@
+"""
+Created on Wed Aug 23 2e:28:33 2017
+
+@author: Dmitry White
+"""
+
+# TODO: Create a function that takes an integer as an argument and
+# returns "Even" for even numbers or "Odd" for odd numbers.
+
+
+def even_or_odd(n):
+    return "Even" if n%2 == 0 else "Odd"
