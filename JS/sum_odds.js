6cf3a26691053e9ab9fce0f04d70efec41293f10
Dmitry White <dmitry.b.white@gmail.com>
2017-09-20 01:31:53 +0300

    Solve Sum of odd numbers

---
+++ JS/sum_odds.js
@@ -0,0 +1,19 @@
+/**
+ * Created on Wed Sep 20 00:39:23 2017
+ * @author: Dmitry White
+ */
+
+ /**
+  * TODO: Given the triangle of consecutive odd numbers:
+  *             1
+  *          3     5
+  *       7     9    11
+  *     13    15    17    19
+  * 21    23    25    27    29
+  * ...
+  * Calculate the row sums of this triangle from the row index (starting at index 1).
+  */
+
+function rowSumOddNumbers(n) {
+	return n*n*n;
+}
