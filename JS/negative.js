57e65ab6c716cdeb87517e7c2df231cbe109761b
Dmitry White <dmitry.b.white@gmail.com>
2017-09-20 01:30:53 +0300

    Solve Negative

---
+++ JS/negative.js
@@ -0,0 +1,13 @@
+/**
+ * Created on Wed Sep 20 00:23:06 2017
+ * @author: Dmitry White
+ */
+
+ /**
+  * TODO: You are given a number and have to make it negative.
+  * The number can be negative already, in which case no change is required.
+  */
+
+function makeNegative(num) {
+    return num <= 0 ? num : -num ;
+}
