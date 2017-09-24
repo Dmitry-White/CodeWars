a15c448648a7885dea6484e5d5a284be4cda7313
Dmitry White <dmitry.b.white@gmail.com>
2017-09-25 01:35:43 +0300

    Solve Longest Common Subsequance

---
+++ JS/4kyu/lCS.js
@@ -0,0 +1,26 @@
+/**
+ * Created on Mon Sep 25 01:34:45 2017
+ * @author: Dmitry White
+ */
+
+ /**
+  * TODO: Write a function called LCS that accepts two sequences and
+  * returns the longest subsequence common to the passed in sequences.
+  */
+
+function LCS(a, b) {
+  var a_len = a.length;
+  var b_len = b.length;
+  var a_sub = a.slice(0, a_len - 1);
+  var b_sub = b.slice(0, b_len - 1);
+
+  if (a_len === 0 || b_len === 0) {
+    return '';
+  } else if (a[a_len - 1] === b[b_len - 1]) {
+    return LCS(a_sub, b_sub) + a[a_len - 1];
+  } else {
+    var x = LCS(a, b_sub);
+    var y = LCS(a_sub, b);
+    return (x.length > y.length) ? x : y;
+  }
+}
