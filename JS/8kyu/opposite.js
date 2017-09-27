e57a7bd02ab9f9813d27b8710cc4622f53494d76
Dmitry White <dmitry.b.white@gmail.com>
2017-09-27 22:07:08 +0300

    Solve Opposite Number

---
+++ JS/8kyu/opposite.js
@@ -0,0 +1,12 @@
+
+/**
+ * Created on Wed Sep 27 22:06:01 2017
+ * @author: Dmitry White
+ */
+
+ /**
+  * TODO: Given a number, find its opposite.
+  */
+function opposite(number) {
+  return -number;
+}
