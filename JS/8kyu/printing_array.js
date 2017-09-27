a4df57e78d1a49bc6751d9e241d623df7e142335
Dmitry White <dmitry.b.white@gmail.com>
2017-09-27 22:20:34 +0300

    Solve Printing Array

---
+++ JS/8kyu/printing_array.js
@@ -0,0 +1,15 @@
+/**
+ * Created on Wed Sep 27 22:18:51 2017
+ * @author: Dmitry White
+ */
+
+ /**
+  * TODO: Input: Array of elements
+  * ["h","o","l","a"]
+  * Output: String with comma delimited elements of the array in th same order.
+  * "h,o,l,a"
+  */
+
+function printArray(array){
+  return array.join(",");
+}
