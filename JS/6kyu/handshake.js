5dd9babb1e8e2040c9895535eac8f513c7f1ed6d
Dmitry White <dmitry.b.white@gmail.com>
2017-09-30 01:11:43 +0300

    Solve Reverse Handshake Problem

---
+++ JS/6kyu/handshake.js
@@ -0,0 +1,25 @@
+/**
+ * Created on Sat Sep 30 01:11:32 2017
+ * @author: Dmitry White
+ */
+
+ /**
+  * TODO: Johnny is a farmer and he annually holds a beet farmers convention
+  * "Drop the beet".
+  * Every year he takes photos of farmers handshaking.
+  * Johnny knows that no two farmers handshake more than once.
+  * He also knows that some of the possible handshake combinations may not happen.
+  * However, Johnny would like to know the minimal amount of people
+  * that participated this year just by counting all the handshakes.
+  * Help Johnny by writing a function, that takes the amount of handshakes
+  * and returns the minimal amount of people needed to perform these handshakes
+  * (a pair of farmers handshake only once).
+  */
+
+function getParticipants(handshakes){
+  var D, num;
+  D = 1 + 4 * 2 * handshakes;
+  num = (1 + Math.sqrt(D))/2;
+  floor_num = Math.floor(num);
+  return (num > floor_num) ? floor_num + 1 : num;
+}
