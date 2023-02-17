5d99156dc59925f747a8e2e637af0d19bfe8353e
Dmitry White <dmitry.b.white@gmail.com>
2023-02-17 20:00:38 +0000

    Solve Two Oldest Ages

---
+++ Go/7kyu/two_oldest_ages.go
@@ -0,0 +1,11 @@
+package kata
+
+import "sort"
+
+func TwoOldestAges(ages []int) [2]int {
+	sort.Slice(ages, func(i, j int) bool {
+		return ages[i] > ages[j]
+	})
+
+	return [2]int{ages[1], ages[0]}
+}
