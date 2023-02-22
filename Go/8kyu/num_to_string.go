98873c93044f1ee7b4add12e5f2b32a3dd22d6b2
Dmitry White <dmitry.b.white@gmail.com>
2023-02-22 18:45:40 +0000

    Solve Num to String

---
+++ Go/8kyu/num_to_string.go
@@ -0,0 +1,15 @@
+/*
+Created on Wed Feb 22 17:12:09 2023
+@author: Dmitry White
+*/
+package kata
+
+import "fmt"
+
+/*
+	TODO: We need a function that can transform a number (integer) into a string.
+*/
+
+func NumberToString(n int) string {
+	return fmt.Sprint(n)
+}
