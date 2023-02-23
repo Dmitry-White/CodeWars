f42fabf075d2cf4a9ee36e1a2721959418c75c85
Dmitry White <dmitry.b.white@gmail.com>
2023-02-23 12:27:31 +0000

    Solve Chuck Norris 7

---
+++ JS/8kyu/chuck_norris_7.js
@@ -0,0 +1,13 @@
+/**
+ * Created on Wed Feb 22 18:09:10 2023
+ * @author: Dmitry White
+ */
+
+/**
+ * TODO: Your task is to be more like Chuck (ha! good luck!).
+ * Write a function that returns false without ever actually using the word false...
+ */
+
+function ifChuckSaysSo() {
+  return !!0;
+}
