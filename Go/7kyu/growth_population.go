ea57017de734920e734aef0a9a8765b4807b4471
Dmitry White <dmitry.b.white@gmail.com>
2023-02-26 16:50:04 +0000

    Solve Growth Population

---
+++ Go/7kyu/growth_population.go
@@ -0,0 +1,24 @@
+/*
+Created on Sun Feb 26 16:30:52 2023
+@author: Dmitry White
+*/
+package main
+
+/*
+	TODO: In a small town the population is `p0` at the beginning of a year.
+	The population regularly increases by `percent`` per year
+	and moreover `aug` new inhabitants per year come to live in the town.
+	How many years does the town need to see its population greater or equal to `p` inhabitants?
+*/
+
+func NbYear(p0 int, percent float64, aug int, p int) int {
+	var n int
+	perYear := (100 + percent) / 100
+	currP := p0
+
+	for n = 0; currP < p; n++ {
+		currP = int(float64(currP)*perYear) + aug
+	}
+
+	return n
+}
