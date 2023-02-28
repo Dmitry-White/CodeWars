fd5ba53a7016a7c3b2aa5251d09134ed10b41b98
Dmitry White <dmitry.b.white@gmail.com>
2023-02-28 20:31:56 +0000

    Refactor Reverse String

--- Go/8kyu/reverse_string.go
+++ Go/8kyu/reverse_string.go
@@ -4,17 +4,15 @@ Created on Tue Feb 28 20:27:53 2023
 */
 package main
 
-import "strings"
-
 /*
 	TODO: Complete the solution so that it reverses the string passed into it.
 */
 func Solution(word string) string {
-	strArr := make([]string, len(word))
+	resultWord := ""
 
-	for i := len(word) - 1; i >= 0; i-- {
-		strArr = append(strArr, string(word[i]))
+	for _, char := range word {
+		resultWord = string(char) + resultWord
 	}
 
-	return strings.Join(strArr, "")
+	return resultWord
 }
