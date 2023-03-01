e07158716026e018547032057bb77347ff52fea2
Dmitry White <dmitry.b.white@gmail.com>
2023-03-01 17:32:33 +0000

    Solve Sum of Angles

---
+++ Go/7kyu/sum_of_angles.go
@@ -0,0 +1,14 @@
+/*
+Created on Wed Mar 01 17:32:44 2023
+@author: Dmitry White
+*/
+package kata
+
+/*
+	TODO: Find the total sum of internal angles (in degrees) in an n-sided simple polygon.
+	N will be greater than 2.
+*/
+
+func Angle(n int) int {
+	return (n - 2) * 180
+}
